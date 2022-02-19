8e9cb19ef15d2d471084244d13cac4669853a42c
Bruno <brunocrysnamum71@gmail.com>
2022-02-19 19:05:38 -0300

    Documentacao da Question02

--- Desafio de programacao/src/br/com/questions/Question02.java
+++ Desafio de programacao/src/br/com/questions/Question02.java
@@ -1,8 +1,19 @@
 package br.com.questions;
 
+/**
+ * Segunda questão do Desafio.
+ * @author Bruno Silva Da Silva
+ *
+ */
 public class Question02 {
 	private static final int MIN_LENGTH = 6;
 	
+	/**
+	 * Verifica o comprimento de uma string e compara com o comprimento mínimo necessário.<br>
+	 * <b>Comprimento mínimo:</b> 6.
+	 * @param password string para ser verificada.
+	 * @return retorna o número mínimo de caracteres para serem adicionados para a string ser considerada segura.
+	 */
 	public static int checkStringLength(String password) {
 		if(password.length() >= MIN_LENGTH) 
 			return 0;
